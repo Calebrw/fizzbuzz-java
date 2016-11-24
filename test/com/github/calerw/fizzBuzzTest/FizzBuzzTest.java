5d70279eac3dc5f927dac0c0d21fc9ae4a4004b6
calebrw <calebrw@gmail.com>
2016-11-23 22:36:52 -0600

    Update tests to use StringBuilder where needed.

--- test/com/github/calerw/fizzBuzzTest/FizzBuzzTest.java
+++ test/com/github/calerw/fizzBuzzTest/FizzBuzzTest.java
@@ -35,12 +35,15 @@ public class FizzBuzzTest {
     @Test
     public void testGenerate() throws IOException {
         FizzBuzz fizzBuzz1 = new FizzBuzz(1);
+        final String result1 = fizzBuzz1.generate().toString();
         FizzBuzz fizzBuzz2 = new FizzBuzz(10);
+        final String result2 = fizzBuzz2.generate().toString();
         FizzBuzz fizzBuzz3 = new FizzBuzz(100);
+        final String result3 = fizzBuzz3.generate().toString();
 
-        assertEquals(fizzBuzz1.generate(), "1");
-        assertEquals(fizzBuzz2.generate(), "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz");
-        assertEquals(fizzBuzz3.generate(), "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz");
+        assertEquals(result1, "1");
+        assertEquals(result2, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz");
+        assertEquals(result3, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz");
     }
 
 }
