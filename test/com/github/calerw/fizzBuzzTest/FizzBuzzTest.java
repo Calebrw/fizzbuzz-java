4f4e3217744f4f7ee062e424e9cad36360c6e338
Calebrw <calebrw@gmail.com>
2021-01-27 00:13:07 -0600

    Add some logging. No reason.

--- test/com/github/calerw/fizzBuzzTest/FizzBuzzTest.java
+++ test/com/github/calerw/fizzBuzzTest/FizzBuzzTest.java
@@ -19,6 +19,10 @@ import com.github.calebrw.fizzbuzz.FizzBuzz;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,6 +31,9 @@ import static org.junit.Assert.assertEquals;
  */
 public class FizzBuzzTest {
 
+    public FizzBuzzTest() {
+    }
+
     /**
      * Tests the {@link FizzBuzz#generate()} method
      *
@@ -34,6 +41,11 @@ public class FizzBuzzTest {
      */
     @Test
     public final void testGenerate() throws IOException {
+
+        Logger logger = Logger.getLogger(FizzBuzz.class.getName());
+        logger.setLevel(Level.FINE);
+        logger.addHandler(new StreamHandler(System.out, new SimpleFormatter()));
+
         FizzBuzz fizzBuzz1 = new FizzBuzz(1);
         final String result1 = fizzBuzz1.generate().toString();
         FizzBuzz fizzBuzz2 = new FizzBuzz(10);
@@ -41,9 +53,13 @@ public class FizzBuzzTest {
         FizzBuzz fizzBuzz3 = new FizzBuzz(100);
         final String result3 = fizzBuzz3.generate().toString();
 
-        assertEquals("1", result1);
-        assertEquals("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz", result2);
-        assertEquals("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz", result3);
+        assertEquals("", "1", result1);
+        logger.log(Level.FINE, "Test 1 Complete.");
+        assertEquals("", "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz", result2);
+        logger.log(Level.FINE, "Test 2 Complete.");
+        assertEquals("", "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz", result3);
+        logger.log(Level.FINE, "Test 3 Complete.");
+
     }
 
 }
